d3e4f9600afdd2aa78922e344a0ad7cb1274fe68
Kai Meikle <kaimeikle123@gmail.com>
2022-03-14 23:47:39 +0000

    added index which groups all functions together

---
+++ index.js
@@ -0,0 +1,25 @@
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const head = require("./head");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const middle = require("./middle");
+const tail = require("./tail");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
+
+module.exports = {
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  head,
+  letterPositions,
+  map,
+  tail,
+  middle,
+  takeUntil,
+  without,
+};
