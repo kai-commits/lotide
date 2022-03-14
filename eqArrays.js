cb4678b920b373eef94409aa121f22b754ae2852
Kai Meikle <kaimeikle123@gmail.com>
2022-03-14 20:53:54 +0000

    refactored test for eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = (arr1, arr2) => {
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
@@ -19,8 +11,4 @@ const eqArrays = (arr1, arr2) => {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
-assertEqual(eqArrays([1, 2, 4], [1, 2, 3]), false);
-assertEqual(eqArrays(['1', 2, 3], [1, 2, 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+module.exports = eqArrays;
