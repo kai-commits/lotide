312323741aa09d6a01fc9d2d19720e219121de9f
Kai Meikle <kaimeikle123@gmail.com>
2022-03-14 22:02:43 +0000

    refactored tests for tail and middle using mocha & chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,22 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case 2: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe('#tail', () => {
+  it('returns correct amount of elements', () => {
+    const result = ["Hello", "Lighthouse", "Labs"];
+    assert.strictEqual(tail(result).length, 2);
+  });
+  it('returns correct first element', () => {
+    const result = ["Hello", "Lighthouse", "Labs"];
+    assert.strictEqual(tail(result)[0], 'Lighthouse');
+  });
+  it('returns correct second element', () => {
+    const result = ["Hello", "Lighthouse", "Labs"];
+    assert.strictEqual(tail(result)[1], 'Labs');
+  });
+  it('doesn\'t alter original array', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+});
