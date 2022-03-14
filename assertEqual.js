a91a185e545ba372b6f2ef0c4a5036a2651f36e0
Kai Meikle <kaimeikle123@gmail.com>
2022-03-14 20:04:45 +0000

    refactored tests for head and assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,4 @@ const assertEqual = (actual, expected) => {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("identical string", "identical string");
-assertEqual(1, 2);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,15 +1,3 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = arr => arr[0];
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
+module.exports = head;
