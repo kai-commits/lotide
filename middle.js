2ea9f6827b33f9e05defd837d41d7f97aef8a744
Kai Meikle <kaimeikle123@gmail.com>
2022-03-14 21:04:10 +0000

    refactored test for middle

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  } else {
-    return false;
-  }
-  return true;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`🟢 Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const middle = (arr) => {
   let midArr = [];
   if (arr.length !== 0 && arr.length !== 1 && arr.length !== 2) {
@@ -31,10 +10,4 @@ const middle = (arr) => {
   return midArr;
 };
 
-// TEST CODE
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([]), []);
+module.exports = middle;
